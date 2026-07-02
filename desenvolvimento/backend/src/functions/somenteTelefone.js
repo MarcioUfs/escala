012c9c65e0416824a7beffb1fe24f0bd60f60cdc
MarcioUfs <devil@academico.ufs.br>
2026-07-02 02:08:09 -0300

    Refactor(hotcode): Modificacao de logica na funcao somenteTelefone

--- desenvolvimento/backend/src/functions/somenteTelefone.js
+++ desenvolvimento/backend/src/functions/somenteTelefone.js
@@ -4,10 +4,11 @@ function somenteTelefone(entrada) {
     if (telefoneValido) {
         re = telefoneValido.replace(/\D/g, '')
         let tamanho = re.length;
-        if(tamanho !== 11 || tamanho !== 10){
-            return 0
-        }else{
+        console.log('Tamanho do telefone:', tamanho, 'Telefone formatado:', re);
+        if(tamanho === 11 || tamanho === 10){
             return re
+        }else{
+            return 0
         }
     }else{
         return 0
