cd323419c4bff9747fea602f6edb8ca60df02ee5
MarcioUfs <devil@academico.ufs.br>
2026-08-04 19:18:04 -0300

    Feat(hotcode):Vite URL para deploy

--- desenvolvimento/frontend/src/services/api.js
+++ desenvolvimento/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.56.1:3000',
+  baseURL: import.meta.env.VITE_API_URL || 'http://192.168.56.1:3000',
   // baseURL: 'http://localhost:3000',
 });
 
